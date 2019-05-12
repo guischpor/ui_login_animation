3d471a90c3afa745885503b511d645a83c4d6c5e
guischpor <guischpor1@live.com>
2019-05-12 14:05:05 -0300

    update

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_login_animation/screens/login/widgets/form_container.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
@@ -13,7 +14,30 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
         body: Container(
       decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('images/background1.jpg')),
+        image: DecorationImage(
+            image: AssetImage('images/background1.jpg'), fit: BoxFit.cover),
+      ),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 70.0,
+                      bottom: 32.0,
+                    ),
+                    child: Image.asset('images/tick_icon_blue.png',
+                        width: 150, height: 150, fit: BoxFit.contain),
+                  ),
+                  FormContainer(),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     ));
   }
---
+++ lib/screens/login/widgets/form_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FormContainer extends StatelessWidget {
+  const FormContainer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
