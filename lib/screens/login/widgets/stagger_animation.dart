eec436e912d4e2d8b2a6bc159ccaa2d139111201
guischpor <guischpor1@live.com>
2019-05-14 20:04:37 -0300

    update

--- lib/screens/login/widgets/stagger_animation.dart
+++ lib/screens/login/widgets/stagger_animation.dart
@@ -7,9 +7,20 @@ class StaggerAnimation extends StatelessWidget {
 
   StaggerAnimation({this.controller})
       : buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(
-            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150)));
+            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150))),
+        buttonZoomOut = Tween(
+          begin: 60.0,
+          end: 1000.0,
+        ).animate(CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.5,
+              1,
+              curve: Curves.bounceOut,
+            )));
 
   final Animation<double> buttonSqueeze;
+  final Animation<double> buttonZoomOut;
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +34,31 @@ class StaggerAnimation extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(bottom: 50),
       child: InkWell(
-        onTap: () {
-          controller.forward();
-        },
-        child: Container(
-            width: buttonSqueeze.value,
-            height: 60,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(161, 27, 147, 1.0),
-              borderRadius: BorderRadius.all(
-                Radius.circular(30.0),
-              ),
-            ),
-            child: _buildInside(context)),
-      ),
+          onTap: () {
+            controller.forward();
+          },
+          child: buttonZoomOut.value <= 60
+              ? Container(
+                  width: buttonSqueeze.value,
+                  height: 60,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(161, 27, 147, 1.0),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30.0),
+                    ),
+                  ),
+                  child: _buildInside(context),
+                )
+              : Container(
+                  width: buttonZoomOut.value,
+                  height: buttonZoomOut.value,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(161, 27, 147, 1.0),
+                      shape: buttonZoomOut.value < 500
+                          ? BoxShape.circle
+                          : BoxShape.rectangle),
+                )),
     );
   }
 
@@ -55,7 +76,7 @@ class StaggerAnimation extends StatelessWidget {
     } else {
       return CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-        strokeWidth: 2.0,
+        strokeWidth: 1.5,
       );
     }
   }
