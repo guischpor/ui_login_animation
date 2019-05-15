6d154614a966914af0578f4640bbea665f98e5ae
guischpor <guischpor1@live.com>
2019-05-15 15:38:04 -0300

    add perfil

--- lib/screens/home/widgets/category_view.dart
+++ lib/screens/home/widgets/category_view.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/material.dart';
 
-class CategoryView extends StatelessWidget {
+class CategoryView extends StatefulWidget {
+  @override
+  _CategoryViewState createState() => _CategoryViewState();
+}
+
+class _CategoryViewState extends State<CategoryView> {
+  final List<String> categories = ['Job', 'Studies', 'Home'];
+
+  int _category = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.keyboard_arrow_left),
-            iconSize: 30,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        IconButton(
+          onPressed: _category > 0 ? selectBackward : null,
+          icon: Icon(
+            Icons.arrow_back_ios,
+          ),
+          color: Colors.white,
+          disabledColor: Colors.white30,
+        ),
+        Text(
+          categories[_category].toUpperCase(),
+          style: TextStyle(
             color: Colors.white,
+            fontSize: 18.0,
+            letterSpacing: 1.2,
+            fontWeight: FontWeight.w300,
           ),
-          Text(
-            'ESTUDOS',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.0,
-                fontWeight: FontWeight.w300),
+        ),
+        IconButton(
+          onPressed: _category < categories.length - 1 ? selectForward : null,
+          icon: Icon(
+            Icons.arrow_forward_ios,
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.keyboard_arrow_right),
-            iconSize: 30,
-            color: Colors.white,
-          ),
-        ],
-      ),
+          color: Colors.white,
+          disabledColor: Colors.white30,
+        ),
+      ],
     );
+  }
+
+  void selectForward() {
+    setState(() {
+      _category++;
+    });
+  }
+
+  void selectBackward() {
+    setState(() {
+      _category--;
+    });
   }
 }
